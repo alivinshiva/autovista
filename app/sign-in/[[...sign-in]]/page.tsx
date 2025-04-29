bbb3d1b53124e0a88278e3861a4a75488aacad03
pratyushsuryapanda <pratyushsuryapanda@gmail.com>
2025-04-29 03:51:25 +0000

    Fix: Center and style sign-in form

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,13 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-    return <SignIn />
+    return (
+        <div className="flex flex-col items-center justify-center h-screen w-full">
+            <div className="w-full max-w-[500px] p-4">
+                <SignIn />
+            </div>
+        </div>
+
+    )
+
 }
